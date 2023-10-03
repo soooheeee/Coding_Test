7d96cf958e8623d854ff88b5119e183ed101ce23
yn_nub <129060841+soooheeee@users.noreply.github.com>
2023-10-03 22:57:31 +0900

    Update 분수 합.py

--- 백준/Silver/1735. 분수 합/분수 합.py
+++ 백준/Silver/1735. 분수 합/분수 합.py
@@ -6,13 +6,8 @@ import math
 a,b =map(int,input().split())
 c,d=map(int,input().split())
 
-if b!=d:
-    max=math.gcd(b*d,(a*d)+(b*c))
-    bunmo=b*d//max
-    bunza=((a*d)+(b*c))//max
-    print(bunza,bunmo)
-else:
-    max=math.gcd(b,(a+c))
-    bunmo=b//max
-    bunza=(a+c)//max
-    print(bunza,bunmo)
+
+max=math.gcd(b*d,(a*d)+(b*c))
+bunmo=b*d//max
+bunza=((a*d)+(b*c))//max
+print(bunza,bunmo)
