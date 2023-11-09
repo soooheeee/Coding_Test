fac4e73d60a46db4c31d0b72fd30c5d3f6aad4e1
yn_nub <129060841+soooheeee@users.noreply.github.com>
2023-11-09 10:57:42 +0900

    Update 문자열.py

--- 백준/Bronze/9086. 문자열/문자열.py
+++ 백준/Bronze/9086. 문자열/문자열.py
@@ -10,3 +10,9 @@ for i in range(n):
     s=list(li[i])
     print(s[0],end='')
     print(s[len(s)-1])
+
+#### 문자열의 가장 끝은 -1 사용하기
+n=int(input())
+for i in range(n):
+	X=list(input())
+	print(X[0]+X[-1])
