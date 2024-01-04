9992dcdd76a061d6c997f354e425251dab9ad853
yn_nub <129060841+soooheeee@users.noreply.github.com>
2024-01-04 13:15:02 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -0,0 +1,11 @@
+import sys
+input =sys.stdin.readline
+
+S = list(input())
+c = 'abcdefghijklmnopqrstuvwxyz'
+
+for i in c:
+    if i in S:
+        print(S.index(i), end =' ')
+    else:
+        print(-1, end=' ')
