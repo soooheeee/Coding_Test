b9421b250a99a74e94a093d67fe4286175598428
yn_nub <129060841+soooheeee@users.noreply.github.com>
2024-02-26 02:16:59 +0900

    Update 단어 뒤집기.py

--- 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.py
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.py
@@ -24,3 +24,23 @@ for i in range(t):
 #     for test_str in test_list:
 #         result += test_str[-1::-1]+' '
 #     print(result)
+
+# 스택 사용
+# import sys
+
+# input = lambda : sys.stdin.readline().rstrip()
+
+# T = int(input())
+# for _ in range(T):
+#     sentence = input().split()
+#     stack = []
+#     for word in sentence:
+#         convert = ''
+#         for ch in word:
+#             stack.append(ch)
+#         # 리스트는 빈 상태일 때, false로 되고 리스트에 요소가 남아 있다면 true로 변경됨
+#         # false 값이 반환되면 while문을 벗어나감
+#         while stack: 
+#             print(stack.pop(), end="")
+#         print(" ", end="")
+#     print()
