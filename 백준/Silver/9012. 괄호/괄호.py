379bf0f73c5a33128f957501d9b1515ba8d1431f
yn_nub <129060841+soooheeee@users.noreply.github.com>
2024-02-26 16:39:24 +0900

    [Silver IV] Title: 괄호, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+
+for i in range(t):
+    stack = []
+    a=input()
+    for j in a:
+        if j == '(':
+            stack.append(j)
+        elif j == ')':
+            if stack:
+                stack.pop()
+            else: 
+                print("NO")
+                break
+    else: 
+        if not stack:
+            print("YES")
+        else: 
+            print("NO")
