174a754e53b53aca300c9a66117384841bfd7e8e
yn_nub <129060841+soooheeee@users.noreply.github.com>
2024-04-17 23:25:47 +0900

    Update 수 찾기.py

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -13,3 +13,31 @@ for num in m_li:
         print('1')
     else:
         print('0')
+
+## 이진 탐색을 재귀로 구현
+
+import sys
+input = sys.stdin.readline
+
+def binary_search(n_li, x, start, end):
+    if start > end:
+        return False
+    mid = ( start + end ) //2
+    if n_li[mid] == x:
+        return True
+    elif n_li[mid] > x:
+        return binary_search(n_li, x, start, mid-1)
+    else:
+        return binary_search(n_li, x, mid+1, end)
+
+n = int(input())
+n_li = sorted(map(int,input().split()))
+
+m = int(input())
+m_li = list(map(int,input().split()))
+
+for m in m_li:
+    if binary_search( n_li, m, 0, n-1):
+        print(1)
+    else:
+        print(0)
